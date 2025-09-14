424820465405e369045c991645e98f4c0ce6a976
azubin <alex.zubin@oracle.com>
2025-09-14 19:41:01 +0530

    added languages.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
+import React from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { languages } from "./languages"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
     const languageElements = languages.map(lang => {
         const styles = {
